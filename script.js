ae0ee410c961ae5282e07e6a6ea0cc51e9111aa9
etozhenerk <etozhenerk@yandex.ru>
2020-11-25 18:15:50 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let money, income, addExpenses, deposit, mission, period;
+let money = 60000,
+  income = 'фриланс',
+  addExpenses = 'Интернет, Такси, Коммуналка',
+  deposit = true,
+  mission = 130000,
+  period = 4;
 
-alert("Hello world!");
-console.log("Hello world!");
+console.log('type of "money": ', typeof money);
+console.log('type of "income": ', typeof income);
+console.log('typeo f "deposit": ', typeof deposit);
+
+console.log('length of "addExpenses": ', addExpenses.length);
+
+console.log(`Период равен ${period} месяца`);
+console.log(`Цель заработать ${mission} рублей`);
+
+addExpenses = addExpenses.toLowerCase().split(', ');
+console.log('addExpenses: ', addExpenses);
+
+let budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
