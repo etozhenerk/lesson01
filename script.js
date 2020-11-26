d1d60e70321310cd8b5c3cd92855ff59363b2406
etozhenerk <etozhenerk@yandex.ru>
2020-11-26 22:07:27 +0300

    Lesson 4

--- script.js
+++ script.js
@@ -47,14 +47,14 @@ console.log(`Бюджет на день ${budgetDay}`);
 
 function getStatusIncome() {
   if (budgetDay >= 1200) {
-    console.log("У вас высокий уровень дохода");
+    return ("У вас высокий уровень дохода");
   } else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log("У вас средний уровень дохода");
+    return ("У вас средний уровень дохода");
   } else if (budgetDay >= 0 && budgetDay < 600) {
-    console.log("К сожалению у вас уровень дохода ниже среднего");
+    return ("К сожалению у вас уровень дохода ниже среднего");
   } else if (budgetDay < 0) {
-    console.log("Что то пошло не так");
+    return ("Что то пошло не так");
   }
 }
 
-getStatusIncome();
+console.log(getStatusIncome());
